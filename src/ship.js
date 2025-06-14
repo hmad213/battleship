b3ebbdac677418b4575bd48b4d41abe43ad3d6bc
hmad213 <142343785+hmad213@users.noreply.github.com>
2025-06-14 06:45:39 -0700

    Add ship class

---
+++ src/ship.js
@@ -0,0 +1,26 @@
+class Ship{
+    constructor(name, position){
+        this.name= name;
+        this.position = position;
+        this.timesHit = 0;
+    }
+
+    hit(){
+        this.timesHit++;
+    }
+
+    isSunk(){
+        return this.timesHit >= this.position.length;
+    }
+
+    contains(position){
+        for(let i = 0; i < this.position.length; i++){
+            if(position[0] == this.position[i][0] && position[1] == this.position[i][1]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+export { Ship }
---
+++ src/ship.test.js
@@ -0,0 +1,23 @@
+import { Ship } from "./ship";
+
+describe("Ship class", () => {
+    test("hit function", () => {
+        let ship = new Ship("destroyer", [[3, 3], [3, 4], [3, 5]]);
+        ship.hit();
+        ship.hit();
+        expect(ship.timesHit).toBe(2);
+    })
+
+    test("sunk function", () => {
+        let ship = new Ship("destroyer", [[3, 3], [3, 4], [3, 5]]);
+        expect(ship.isSunk()).toBe(false);
+        ship.timesHit = 3;
+        expect(ship.isSunk()).toBe(true);
+    })
+
+    test("contain function", () => {
+        let ship = new Ship("destroyer", [[3, 3], [3, 4], [3, 5]]);
+        expect(ship.contains([5,5])).toBe(false);
+        expect(ship.contains([3, 3])).toBe(true);
+    })
+})
