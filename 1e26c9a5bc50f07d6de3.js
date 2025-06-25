ebcadec853729b9fc33ba0092ad632380a51ca47
hmad213 <142343785+hmad213@users.noreply.github.com>
2025-06-25 22:02:59 +0500

    Deployment commit

---
+++ 1e26c9a5bc50f07d6de3.js
@@ -0,0 +1,17 @@
+import "./style.css"
+import {User, Computer} from "./components/player.js"
+import { Renderer } from "./components/ui.js";
+
+let r = new Renderer();
+r.showStartMenu();
+
+let u;
+let c;
+
+function createPlayers(userName){
+    u = new User(userName);
+    c = new Computer("Computer");
+    return [u, c];
+}
+
+export {createPlayers}
